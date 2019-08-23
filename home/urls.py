fb8a4b37143cd60e81f675b5c329a8f68089faea
jayzau <zausang@163.com>
2019-08-23 15:21:03 +0800

    重新整理

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from home import views
+
+urlpatterns = [
+    # 首页样式
+    # url(r'^zau_index$', views.zau_index, name='zau_index'),
+    # url(r'^zau_index_nosidebar$', views.zau_index_nosidebar, name='zau_index_nosidebar'),
+    # url(r'^zau_index_noslider$', views.zau_index_noslider, name='zau_index_noslider'),
+    # # 首页图片库展示
+    # url(r'^zau_pict$', views.zau_pict, name='zau_pict'),
+    # # 文章时间线展示
+    # url(r'^zau_timeline$', views.zau_timeline, name='zau_timeline'),
+]
